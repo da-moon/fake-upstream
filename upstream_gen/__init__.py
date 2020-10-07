5d2410bd0819c70b3fdfe204de26a997a7279509
da-moon <damoon.azarpazhooh@live.com>
2020-10-06 23:25:01 -0400

    Update __init__.py

--- upstream_gen/__init__.py
+++ upstream_gen/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 import pbr.version
 
-version_info = pbr.version.VersionInfo('upstream')
+version_info = pbr.version.VersionInfo('upstream_gen')
 __version__ = version_info.release_string()
